7d6c62a64725e27c2cfadf7a35bf0e5c458f150a
Everton Takashi Nishiyama <etnishiyama@gmail.com>
2020-04-09 15:55:19 -0300

    Persist dynamodb local to frameworks path

--- config/project_dependencies.ts
+++ config/project_dependencies.ts
@@ -1,8 +1,8 @@
 'use strict';
 
 import {I18nLocaleService} from "../frameworks/locale/i18n_locale_service";
-import {DynamoDatabaseService} from "../frameworks/persistence/dynamo/dynamo_database_service";
-import {DynamoOfflineDatabaseService} from "../frameworks/persistence/dynamo/dynamo_offline_database_service";
+import {DynamoDatabaseService} from "../frameworks/persistence/dynamodb/dynamo_database_service";
+import {DynamoOfflineDatabaseService} from "../frameworks/persistence/dynamodb/dynamo_offline_database_service";
 import {SqsOfflineService} from "../frameworks/queue/sqs/sqs_offline_service";
 import {SqsService} from "../frameworks/queue/sqs/sqs_service";
 
