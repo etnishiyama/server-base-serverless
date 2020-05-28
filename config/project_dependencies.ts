d9b01639c0dcded34e0acd2239bac428ba38b432
Everton Takashi Nishiyama <etnishiyama@gmail.com>
2020-05-28 11:40:50 -0300

    Remove unnecessary test files

--- config/project_dependencies.ts
+++ config/project_dependencies.ts
@@ -1,5 +1,4 @@
 import {I18nLocaleService} from "../frameworks/locale/i18n_locale_service";
-import {I18nLocaleServiceMocha} from "../frameworks/locale/i18n_locale_service_mocha";
 import {DynamoDatabaseService} from "../frameworks/persistence/dynamodb/dynamo_database_service";
 import {DynamoOfflineDatabaseService} from "../frameworks/persistence/dynamodb/dynamo_offline_database_service";
 import {SqsOfflineService} from "../frameworks/queue/sqs/sqs_offline_service";
@@ -7,6 +6,6 @@ import {SqsService} from "../frameworks/queue/sqs/sqs_service";
 import {HttpRequestService} from "../frameworks/request/http/http_request_service";
 
 export const databaseService = process.env.ENV == 'dev' ? new DynamoOfflineDatabaseService() : new DynamoDatabaseService();
-export const localeService = process.env.ENV == 'test'? new I18nLocaleServiceMocha(): new I18nLocaleService();
+export const localeService = new I18nLocaleService();
 export const requestService = new HttpRequestService();
 export const queueService = process.env.ENV == 'dev' ? new SqsOfflineService() : new SqsService();
