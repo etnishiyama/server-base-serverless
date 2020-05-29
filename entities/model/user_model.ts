9b55410ef6ba7ba62313fc2cf01b2f1be0ce7afb
Everton Takashi Nishiyama <etnishiyama@gmail.com>
2020-05-29 12:22:42 -0300

    Add dynamo models to base

--- entities/model/user_model.ts
+++ entities/model/user_model.ts
@@ -2,6 +2,11 @@
  * User POJO class.
  */
 export type User = {
+  id?: string;
   fullName?: string;
   email: string;
+  createdAt?: string;
+  updatedAt?: string;
+  deletedAt?: string;
+  isActive?: boolean;
 }
